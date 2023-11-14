607b9e3c393e512326745dcf65463bd9472ca357
Macksontibande <Macksontibade@gmail.com>
2023-11-14 12:07:51 +0000

    8. Primitive - Holberton Class & 9. Hoisting

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,31 @@
+// Implement a class named HolbertonClass.
+export default class Airport {
+  /**
+   * size (Number)
+   * location (String)
+   */
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
