1371dfa811db6985f2df363531197b7be1ededf7
Macksontibande <Macksontibade@gmail.com>
2023-11-14 07:54:49 +0000

    0. You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+// Assigned to _maxStudentsSize after accepting it.
+export default class ClassRoom {
+  /**
+   * Has a class named ClassRoom
+   * @param {Number} maxStudentsSize - Is max no. of students.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
