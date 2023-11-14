bd359cb5b8a3defde0af1c111b6842786b034179
Macksontibande <Macksontibade@gmail.com>
2023-11-14 08:01:20 +0000

    task 1. qand 2.

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,12 @@
+export default class HolbertonCourse {
+  /**
+   * Creates a new HolbertonCourse class
+   * @param {String}, @param {Number} and @param {String[]}.
+   *
+   * Implement getters and setters for each attribute
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
