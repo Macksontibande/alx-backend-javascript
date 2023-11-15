59e5ca3a88a8d3c04b09503a4eca8f9896997ed7
Macksontibande <Macksontibade@gmail.com>
2023-11-15 07:43:41 +0000

    10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+// A function named updateUniqueItems that returns an updated map.
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    // If the value is 1, update it to 100
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
