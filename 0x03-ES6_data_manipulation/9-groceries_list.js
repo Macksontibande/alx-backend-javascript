d790c2107992ea47b7841f4dda175b36853e5558
Macksontibande <Macksontibade@gmail.com>
2023-11-15 07:41:11 +0000

    taske 9. and 11. Weak link data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * A function named groceriesList that returns a map of groceries
+ *
+ * @return {Map} A Map of groceries with their quantities
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
