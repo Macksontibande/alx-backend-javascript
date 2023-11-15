111e723731f7266282c2ecba06ad17b57166f9b9
Macksontibande <Macksontibade@gmail.com>
2023-11-15 07:31:03 +0000

    6. Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * A function named setFromArray that returns a Set from an array.
+ *
+ * @param {Array} and @return {Set}.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
